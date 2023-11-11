7f90d70fb4e7dd1981a0f9e74f9dae1bd1017e61
Alejandra Duran <ad2002@princeton.edu>
2023-11-11 18:02:03 -0500

    test analog output

--- control_code/build.py
+++ control_code/build.py
@@ -7,39 +7,15 @@ import time
 # acquisition of a camera with the generation of an analog signal to control a 
 # galvo mirror and digital signals to control 3 lasers. 
 
-import numpy as np
-import nidaqmx
+test_Task = nidaqmx.Task()
+test_Task.ao_channels.add_ao_voltage_chan('myDAQ1/ao1')
+test_Task.timing.cfg_samp_clk_timing(rate= 80, sample_mode= nidaqmx.constants.AcquisitionType.FINITE, samps_per_chan= 40)
 
-def generate_custom_signal(task, frequency, amplitude, duration):
-    # Generate a time array for the signal
-    time_array = np.linspace(0, duration, int(duration * frequency), endpoint=False)
+test_Writer = nidaqmx.stream_writers.AnalogSingleChannelWriter(test_Task.out_stream, auto_start=True)
 
-    # Generate a custom analog output signal (e.g., a sine wave)
-    analog_output_signal = amplitude * np.sin(2 * np.pi * frequency * time_array)
+samples = np.append(5*np.ones(30), np.zeros(10))
 
-    # Configure the analog output task
-    task.ao_channels.add_ao_voltage_chan("Dev1/ao0", min_val=-10.0, max_val=10.0)
-    task.timing.cfg_samp_clk_timing(rate=frequency, active_edge=nidaqmx.constants.Edge.RISING, sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
-
-    # Write the custom analog output signal to the analog output channel
-    task.write(analog_output_signal, auto_start=True)
-
-    # Run the task
-    task.start()
-
-    # Wait for the specified duration
-    task.wait_until_done(timeout=duration)
-
-    # Stop and clear the task
-    task.stop()
-    task.close()
-
-if __name__ == "__main__":
-    # Define the parameters of the custom analog output signal
-    frequency = 1000.0  # Hz
-    amplitude = 5.0  # Volts
-    duration = 5.0  # Seconds
-
-    # Create a NIDAQmx task for analog output
-    with nidaqmx.Task() as task:
-        generate_custom_signal(task, frequency, amplitude, duration)
+test_Writer.write_many_sample(samples)
+test_Task.wait_until_done()
+test_Task.stop()
+test_Task.close()
